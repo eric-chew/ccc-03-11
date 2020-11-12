79fa0e436084d41350603cec1509ff99d6f74a25
eric-chew <ericyjchew@gmail.com>
2020-11-12 01:00:51 +0000

    Added db commands

---
+++ commands.py
@@ -0,0 +1,29 @@
+from main import db
+from flask import Blueprint
+
+db_commands = Blueprint('db', __name__)
+
+@db_commands.cli.command('create')
+def create_db():
+    db.create_all()
+    print('Tables Created')
+    
+@db_commands.cli.command('drop')
+def drop_db():
+    db.drop_all()
+    print('Tables Dropped')
+    
+@db_commands.cli.command('seed')
+def seed_db():
+    from models.Book import Book
+    from faker import Faker
+    faker = Faker()
+    
+    for i in range(10):
+        book = Book()
+        book.title = faker.catch_phrase()
+        db.session.add(book)
+        print(f'Book record #{i+1} created')
+    
+    db.session.commit()
+    print('Tables Seeded')
